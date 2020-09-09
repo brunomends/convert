bbc4eaecc499bbbe89957ced371b3a32d7403ab7
BrunoMendes <brunomends02@outlook.com>
2020-09-08 22:55:17 -0300

    Add Materialize

--- convert.js
+++ convert.js
@@ -1,30 +1,34 @@
-console.log('AQUI!!!!!')    
-let url = "http://economia.awesomeapi.com.br/json/USD-BRL"
-// fetch(url).
-// then(response => response.json())
-// .then(jsonBody => {})
-//document.querySelector("#app").innerHTML = jsonBody
+let qtde = document.getElementById('us-inputTxt')
+let url = `http://economia.awesomeapi.com.br/json/all/USD-BRL,USDT-BRL,CAD-BRL,AUD-BRL,EUR-BRL,BTC-BRL,ARS-BRL`
+ function limpa() {
+    document.querySelector('.main').innerHTML = ''
+ }
 
-async function getContent(){
+async function getContent() {
+    
+    limpa()
+    
     try {
         const response = await fetch(url)
         const data = await response.json()
-        console.log(data)
-        show(data)  
+        const result = Object.values(data)
+
+        //O método map percorre o array e executa uma ação para cada item
+
+        result.map(moeda => { 
+            // Adicionando os elementos na tag ul
+            let venda = qtde.value * moeda.ask
+            let compra = qtde.value * moeda.bid
+            document.querySelector('.main')
+            .innerHTML += `<li>${moeda.name}</li>` + `<li>Valor de venda: R$ ${venda.toFixed(2)}</li>` + `<li>Valor de compra: R$ ${compra.toFixed(2)}</li>` + `<br>`
+        })
+
+        // O método toFixed fixa duas casas decimais após a vírgula no valor
+
     } catch (error) {
         console.log(error)
     }
+
 }
 
-getContent()
-
-function show (moeda) {
-    let output = ''
-
-    for ( let m of moeda) {
-        output += `<li>${m.name}</li>`
-    }
-
-    document.querySelector('main').innerHTML = output
-}
 
